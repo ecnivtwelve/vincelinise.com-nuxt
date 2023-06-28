514a87eb911f9ffa1b88214334a2367289a71010
Vince Linise <vincelinise@gmail.com>
2023-06-28 23:17:28 +0200

    Ajout de l'optimisation des images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/content'
+    '@nuxt/content',
+    '@nuxt/image',
   ],
   devtools: { enabled: true },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  image: {
+    inject: true,
   },
 })
